bb3974b0858605b137698f4cc3e11ef415e489df
marco <mazi2001@student.miun.se>
2022-11-10 23:02:24 +0100

    Add the imports for Redux and Redux persist

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-// Importerar React, ReactDOM och "huvudkomponenten"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/index';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
-// Renderar appen i index.html
 const root = ReactDOM.createRoot(document.getElementById('page-wrapper'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </React.StrictMode>
+  </Provider>
 );
