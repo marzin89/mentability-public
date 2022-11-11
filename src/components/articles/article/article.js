d0993cf566be6c0834fc540823ad1142450ec493
marco <mazi2001@student.miun.se>
2022-11-11 01:08:03 +0100

    Rename the render components for articles to article.js and change it to a function component

---
+++ src/components/articles/article/article.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { articleActions } from '../../../store/slices/article-slice';
+
+function Article(props) {
+    const dispatch = useDispatch();
+
+    return (
+        <article key={props.article.id} id={props.article.id} 
+            className={`news-article ${props.class}`}>
+            <h3>{props.article.title}</h3>
+            <p className="date">{props.article.date.slice(0, 10)}</p>
+            <p>{props.article.content.slice(0, 150) + ' ...'}</p>
+            <p className="author">{props.article.author}</p>
+            <Link id={`article${props.article.id}`} className="find-out-more" 
+                to="/article" onClick={(e) => dispatch(articleActions.getArticle(
+                    e.currentTarget.parentElement.id))}>Läs mer</Link>
+        </article>
+
+    )
+}
+
+export default Article;
