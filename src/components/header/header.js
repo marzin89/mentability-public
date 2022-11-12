e39f5925ae20b1a38eba888362b2c7f67e81f8d8
marco <mazi2001@student.miun.se>
2022-11-12 13:57:43 +0100

    Turn the header componentn into a function component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,81 +1,59 @@
-// Importerar React
-import React from "react";
+import { useState } from 'react';
 import logo from '../../images/logo.jpg';
 
-// Sidhuvud
-class Header extends React.Component {
-    // Konstruktor
-    constructor(props) {
-        super(props);
+function Header() {
+    const [showNav, setShowNav] = useState(false);
 
-        this.handleNavIconClick = this.handleNavIconClick.bind(this);
+    function toggleNav(e, value) {
+        e.preventDefault();
+        setShowNav(value);
     }
 
-    // Rendering
-    render() {
-        return (
-            <header>
-                <div id="header-wrapper">
-                    {/* Logotyp */}
-                    <a href="/" id="logo" role="button" aria-label="Länk till startsidan">
-                        <img src={logo} alt="Mentabilitys logotyp" />
-                    </a>
-                    <div id="header-right">
-                        {/* Sökruta */}
-                        <form id="search" action="index.html" method="post">
-                            <label htmlFor="search-bar">Sök</label>
-                            <input id="search-bar" type="search"></input>
-                            <input id="search-btn" className="button" type="submit" value="Sök"></input>
-                        </form>
-                        {/* Hamburgerikon */}
-                        <div id="nav-icon" role="button" aria-label="Hamburgerikon" aria-expanded="false"
-                            onClick={this.handleNavIconClick}>
-                            <div id="bar1" className="bar"></div>
-                            <div id="bar2" className="bar"></div>
-                            <div id="bar3" className="bar"></div>
-                        </div>
-                        {/* Desktopnavigering */}
-                        <nav id="main-nav-desktop" className="main-nav">
-                            <ul>
-                                <li><a className="navlink" href="index.html#main">Start</a></li>
-                                <li><a className="navlink" href="index.html#about">Om oss</a></li>
-                                <li><a className="navlink" href="index.html#news">Nyheter</a></li>
-                                <li><a className="navlink" href="index.html#activities">Aktiviteter</a></li>
-                                <li><a className="navlink" href="index.html#contact-form">Kontakt</a></li>
-                                <li><a className="navlink" href="https://localhost:7096/">Logga in</a></li>
-                            </ul>
-                        </nav>
+    return (
+        <header>
+            <div id="header-wrapper">
+                <a href="/" id="logo" role="button" aria-label="Länk till startsidan">
+                    <img src={logo} alt="Mentabilitys logotyp" />
+                </a>
+                <div id="header-right">
+                    <form id="search" action="index.html" method="post">
+                        <label htmlFor="search-bar">Sök</label>
+                        <input id="search-bar" type="search"></input>
+                        <input id="search-btn" className="button" type="submit" value="Sök"></input>
+                    </form>
+                    <div id="nav-icon" role="button" aria-label="Hamburgerikon" aria-expanded="false"
+                        onClick={(e) => toggleNav(e, true)}>
+                        <div id="bar1" className="bar"></div>
+                        <div id="bar2" className="bar"></div>
+                        <div id="bar3" className="bar"></div>
                     </div>
-                    {/* Mobilnavigering */}
-                    <nav id="main-nav-mobile" className="main-nav">
+                    <nav id="main-nav-desktop" className="main-nav">
                         <ul>
-                            <li><a className="navlink" href="index.html">Start</a></li>
+                            <li><a className="navlink" href="index.html#main">Start</a></li>
                             <li><a className="navlink" href="index.html#about">Om oss</a></li>
                             <li><a className="navlink" href="index.html#news">Nyheter</a></li>
                             <li><a className="navlink" href="index.html#activities">Aktiviteter</a></li>
                             <li><a className="navlink" href="index.html#contact-form">Kontakt</a></li>
                             <li><a className="navlink" href="https://localhost:7096/">Logga in</a></li>
-                            <li><a className="navlink" href="#" onClick={this.handleCloseLinkClick}>Stäng</a></li>
                         </ul>
                     </nav>
                 </div>
-            </header>
-        )
-    }
-
-    // Visar hamburgermenyn och uppdaterar aria-expanded
-    handleNavIconClick(e) {
-        document.getElementById('main-nav-mobile').style.display = 'block';
-        e.target.setAttribute('aria-expanded', true);
-    }
-
-    // Döljer hamburgermenyn och uppdaterar aria-expanded
-    handleCloseLinkClick(e) {
-        e.preventDefault();
-        document.getElementById('main-nav-mobile').style.display = 'none';
-        document.getElementById('nav-icon').setAttribute('aria-expanded', false);
-    }
+                {showNav ? 
+                <nav id="main-nav-mobile" className="main-nav">
+                    <ul>
+                        <li><a className="navlink" href="index.html">Start</a></li>
+                        <li><a className="navlink" href="index.html#about">Om oss</a></li>
+                        <li><a className="navlink" href="index.html#news">Nyheter</a></li>
+                        <li><a className="navlink" href="index.html#activities">Aktiviteter</a></li>
+                        <li><a className="navlink" href="index.html#contact-form">Kontakt</a></li>
+                        <li><a className="navlink" href="https://localhost:7096/">Logga in</a></li>
+                        <li><a className="navlink" href="#" onClick={(e) => toggleNav(e, false)}>
+                            Stäng</a></li>
+                    </ul>
+                </nav> : null}
+            </div>
+        </header>
+    );
 }
 
-// Exporterar komponenten
 export default Header;
