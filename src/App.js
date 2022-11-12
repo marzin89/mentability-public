cf98b6f0c6aeb76c6da628873bc5efbf8d016d4d
marco <mazi2001@student.miun.se>
2022-11-12 01:22:25 +0100

    Fetch all articles, activities and quotes if they are not already in the Redux store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-// Importerar komponenter
+import { useSelector, useDispatch } from 'react-redux';
+import { articleActions } from './store/slices/article-slice';
+import { activityActions } from './store/slices/activity-slice';
+import { quoteActions } from './store/slices/quote-slice';
 import Header from './components/header/header';
 import Home from './pages/home/home';
 import Article from './pages/article/article';
 import Activity from './pages/activity/activity';
 import Footer from './components/footer/footer';
 
-// Importerar React Router för routing
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navlink,
-  Navigate
 } from 'react-router-dom';
 
-/* "Huvudkomponenten" som renderar webbplatsen med router för 
-  startsidan och undersidorna. Startsidan har en fallback-route */
 function App() {
+  const quotes     = useSelector((state) => state.quote.quotes);
+  const articles   = useSelector((state) => state.article.articles);
+  const activities = useSelector((state) => state.activity.activities);
+  const dispatch   = useDispatch();
+
+  if (!quotes) {
+    fetch('https://localhost:7076/api/Quote')
+    .then(response => response.json())
+    .then((data) => {
+        const min    = 0;
+        const max    = data.length;
+        const random = Math.floor(Math.random() * (max - min) + min);
+  
+        dispatch(quoteActions.getQuotes(data));
+        dispatch(quoteActions.setRandomQuote(data[random]));
+    })
+    .catch(() => {
+      dispatch(quoteActions.setErrorMessage(
+        'Ett serverfel har uppstått.' + 
+        ' Innehållet kan inte visas för tillfället. Försök igen lite senare.'
+      ));
+    });
+  }
+
+  if (!articles) {
+    fetch('https://localhost:7076/api/NewsArticle')
+    .then(response => response.json())
+    .then((data) => {
+        dispatch(articleActions.getArticles(data));
+    });
+  }
+
+  if (!activities) {
+    fetch('https://localhost:7076/api/Activity')
+    .then(response => response.json())
+    .then((data) => {
+        dispatch(activityActions.getActivities(data));
+    });
+  }
+
   return (
     <div className="App">
       <Router>
@@ -33,5 +71,4 @@ function App() {
   );
 }
 
-// Exporterar komponenten
 export default App;
