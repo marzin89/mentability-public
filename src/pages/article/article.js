8cb000421fa2448ddaf743f7060897ef7fae72b3
marco <mazi2001@student.miun.se>
2022-11-12 14:54:53 +0100

    Remove some old functions

--- src/pages/article/article.js
+++ src/pages/article/article.js
@@ -5,7 +5,6 @@ import Quote from '../../components/quotes/quote';
 function Article() {
     const article = useSelector((state) => state.article.article);
     const text = useSelector((state) => state.article.text);
-    console.log(text);
 
     useEffect(() => {
         document.title = article.title;
@@ -28,44 +27,6 @@ function Article() {
             </div>
         </main>
     );
-
-    /* Funktionen hämtar nyhetsinlägget, ändrar sidans namn 
-        och lagrar inlägget i state */
-    /*
-    getArticle() {
-        const id = localStorage.getItem('articleId');
-
-        fetch(`https://localhost:7076/api/NewsArticle/${id}`)
-        .then(response => response.json())
-        .then((data) => {         
-            document.title = data.title;
-            
-            this.setState({
-                title:   data.title,
-                date:    data.date,
-                content: data.content,
-                imgUrl:  data.imageUrl,
-                author:  data.author,
-            })
-        })
-    }
-
-    /* Funktionen delar upp textinnehåll med flera stycken i en array
-        och returnerar den */
-        /*
-        renderContentMultipleParagraphs() {
-            let content = this.state.content.split('\r\n\r\n');
-            let render  = [];
-    
-            content.map((paragraph) => {
-                render.push(
-                    <p>{paragraph}</p>
-                )
-            })
-    
-            return render;
-        }
-        */
 }
 
 export default Article;
