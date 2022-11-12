e9f4b47434ba8c8a3c29c9137ba1ebbda18ae4a8
marco <mazi2001@student.miun.se>
2022-11-12 14:26:41 +0100

    Turn the article page component into a function component

--- src/pages/article/article.js
+++ src/pages/article/article.js
@@ -1,51 +1,37 @@
-// Importerar React och komponenten som genererar slumpmässiga citat
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Quote from '../../components/quotes/quote';
 
-// Undersida för nyhetsinlägg
-class Article extends React.Component {
-    // Konstruktor som hämtar och lagrar artikeln
-    constructor() {
-        super();
+function Article() {
+    const article = useSelector((state) => state.article.article);
+    const text = useSelector((state) => state.article.text);
+    console.log(text);
 
-        this.setState = this.setState.bind(this);
-        this.renderContentMultipleParagraphs = this.renderContentMultipleParagraphs.bind(this);
+    useEffect(() => {
+        document.title = article.title;
+    });
 
-        this.state = {
-            title:   '',
-            date:    '',
-            imgUrl:  '',
-            content: '',
-            author:  '',
-        }
-
-        this.getArticle();
-    }
-
-    // Rendering
-    render() {
-        return (
-            <main className="main-subpage">
-                {/* Slumpmässigt genererat citat */}
-                <Quote />
-                <div className="gray-section">
-                    {/* Nyhetsinlägg */}
-                    <h1>{this.state.title}</h1>
-                    <div className="article">
-                        <p className="date">{this.state.date.slice(0, 10)}</p>
-                        {/* Om innehållet består av flera stycken, körs funktionen som renderar innehållet som 
-                            en array med stycken. Annars skrivs innehållet ut rakt av. */}
-                        {this.state.content.indexOf('\r\n\r\n') > 0 ? this.renderContentMultipleParagraphs()
-                        : <p>{this.state.content}</p>}
-                        <p className="author">{this.state.author}</p>
-                    </div>
+    return (
+        <main className="main-subpage">
+            <Quote />
+            <div className="gray-section">
+                <h1>{article.title}</h1>
+                <div className="article">
+                    <p className="date">{article.date.slice(0, 10)}</p>
+                    {text.map((paragraph) => {
+                        return (
+                            <p>{paragraph}</p>
+                        )
+                    })}
+                    <p className="author">{article.author}</p>
                 </div>
-            </main>
-        )
-    }
+            </div>
+        </main>
+    );
 
     /* Funktionen hämtar nyhetsinlägget, ändrar sidans namn 
         och lagrar inlägget i state */
+    /*
     getArticle() {
         const id = localStorage.getItem('articleId');
 
@@ -66,6 +52,7 @@ class Article extends React.Component {
 
     /* Funktionen delar upp textinnehåll med flera stycken i en array
         och returnerar den */
+        /*
         renderContentMultipleParagraphs() {
             let content = this.state.content.split('\r\n\r\n');
             let render  = [];
@@ -78,7 +65,7 @@ class Article extends React.Component {
     
             return render;
         }
+        */
 }
 
-// Exporterar komponenten
 export default Article;
