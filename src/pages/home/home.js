328cb3b9fe3dbe997042f0252432a640ac57a2b8
marco <mazi2001@student.miun.se>
2022-11-12 17:18:30 +0100

    Remove the about section and import the about component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,8 @@
 import Quote from '../../components/quotes/quote';
 import Articles from '../../components/articles/articles';
+import About from '../../components/about/about';
 import Activities from '../../components/activities/activities';
 import Contact from '../../components/contact/contact';
-import about from '../../images/about.png';
 
 function Home() {
     return (
@@ -11,31 +11,7 @@ function Home() {
             <section className="gray-section">
                 <h1>Välkommen till Mentability!</h1>
                 <Articles />
-                <section id="about">
-                    <h2>Om oss</h2>
-                    <div className="row">
-                        <div id="about-left">
-                            <p>Mentability är mötesplatsen för dig som behöver en ljus stund i tillvaron.
-                                Vi vänder oss i första hand till personer som lider av psykisk ohälsa,
-                                men alla är välkomna, oavsett vilka förutsättningar du har.
-                                På vårt klubbhus kan du delta i aktiviteter, fika, spela spel eller
-                                prata med likasinnade. Vi finns till för varandra.
-                            </p>
-                            <p>Mentability består av ett 50-tal personer där många har psykiska diagnoser 
-                                av olika slag. Verksamheten drivs ideellt och många av oss har andra jobb 
-                                vid sidan om.
-                            </p>
-                            <p>Nedan ser du kommande aktiviteter. Alla aktiviteter är gratis. 
-                                Vissa aktiviteter kräver anmälan senast en vecka i förväg av planeringsskäl.
-                                Anmälan sker via kontaktformuläret.</p>
-                            <p>Vi på Mentability ser fram emot att få träffa dig!</p>
-                        </div>
-                        <div id="about-right">
-                            <img id="about-image" src={about} alt="En hjärna med två armar som spänner musklerna">
-                            </img>
-                        </div>
-                    </div>
-                </section>
+                <About />
                 <Activities />
                 <Contact />
             </section>
